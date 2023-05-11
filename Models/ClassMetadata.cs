cb5586658bfd583ea736ad11586624d0ffdb0cc9
Kirill <kirillevstunichev@gmail.com>
2023-05-11 13:09:44 +0300

    RequestConfig, RequestHandlerConfig, RequestTextTemplate,, RequestHandlerTextTemplate

---
+++ Models/ClassMetadata.cs
@@ -0,0 +1,13 @@
+﻿namespace Models;
+
+public class ClassMetadata
+{
+    public string[]? Usings { get; set; }
+    public string? Namespace { get; set; }
+    public string? ClassName { get; set; }
+    public string[]? Constructor { get; set; }
+    public string[]? BaseConstructor { get; set; }
+    public string[]? InjectedInfrastructure { get; set; }
+    public string[]? InjectedProperties { get; set; }
+    public string[]? Properties { get; set; }
+}
