877d1dd127c3ec7cca9bea77828c14454294f1ec
Andrey Andrianov <dushesx73@yandex.ru>
2023-06-07 23:18:53 +0300

    draft Append to file

--- Templating/Infra/FileLoader.cs
+++ Templating/Infra/FileLoader.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Templating.Infra;
 
@@ -22,14 +23,44 @@ internal class FileLoader
 
 internal class FileWriter
 {
-    public void AddFileToProject(string fileDirectory, string fileName, string fileContent)
+    public void Write(string fileDirectory, string fileName, string fileContent)
     {
         Directory.CreateDirectory(fileDirectory);
 
-        using (var file = File.Open($"{fileDirectory}\\{fileName}", FileMode.OpenOrCreate))
+        var path = $"{fileDirectory}\\{fileName}";
+
+        using (var file = File.Open(path, FileMode.Create))
         {
             var bytes = Encoding.UTF8.GetBytes(fileContent);
             file.Write(bytes, 0, bytes.Length);
         }
     }
+
+    public void Append(string fileDirectory, string fileName, FileAppendArea area, string content)
+    {
+        var path = $"{fileDirectory}\\{fileName}";
+        
+        if (!File.Exists(path))
+        {
+            // TODO: В перспективе прикрутить логги
+            throw new FileNotFoundException();
+        }
+
+        var lines = File.ReadAllLines(path);
+        
+        if (area == FileAppendArea.Usings)
+        {
+            var usings = lines.Where(x => x.StartsWith("using")).ToList();
+            usings.Add(content);
+
+            // TODO: Это может занять тысячу памяти, нужно потестить
+            usings = usings.OrderBy(x => x).ToList();
+        }
+    }
+}
+
+internal enum FileAppendArea
+{
+    Usings = 0,
+
 }
