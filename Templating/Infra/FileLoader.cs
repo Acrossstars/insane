132189f76ddcbeb727a2f36fd263e43a7a48c7b0
Andrey Andrianov <dushesx73@yandex.ru>
2023-06-07 22:28:15 +0300

    draft FileWriter

--- Templating/Infra/FileLoader.cs
+++ Templating/Infra/FileLoader.cs
@@ -19,3 +19,17 @@ internal class FileLoader
         }
     }
 }
+
+internal class FileWriter
+{
+    public void AddFileToProject(string fileDirectory, string fileName, string fileContent)
+    {
+        Directory.CreateDirectory(fileDirectory);
+
+        using (var file = File.Open($"{fileDirectory}\\{fileName}", FileMode.OpenOrCreate))
+        {
+            var bytes = Encoding.UTF8.GetBytes(fileContent);
+            file.Write(bytes, 0, bytes.Length);
+        }
+    }
+}
